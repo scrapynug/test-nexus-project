f6cf8261e5f56f76a720183326f25a66d82d6224
ChujunJie <honestboychujunjie@163.com>
2023-11-17 17:59:37 +0800

    the project use for testing already created

---
+++ test_nexus_eapi/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $mule-application;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
